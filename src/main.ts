5a86b414f3886dfab504565b70ba1227a9f868be
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2023-05-05 02:10:23 -0300

    adicionado typeorm e configurações iniciais com arquivo .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { PlacesModule } from './places/places.module';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(PlacesModule);
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
